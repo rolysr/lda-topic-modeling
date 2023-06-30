7c009063fdde75fd89c88386d2c0fef701c18057
leandro-driguez <leandro_driguez@outlook.com>
2023-06-30 16:54:30 -0400

    fix: parsing text correctly and remove unwanted chars

--- CoherenceTestIwor.py
+++ CoherenceTestIwor.py
@@ -1,81 +1,67 @@
+import re
 import nltk
-
-#Given an object named 'texts' (a list of tokenized texts, ie a list of lists of tokens)
-#from gensim.test.utils import common_texts as texts
-
-# Create a corpus from a list of lists of tokens
+from nltk.corpus import stopwords
+from gensim.models import LdaModel
+from collections import defaultdict
+from gensim.models import CoherenceModel
 from gensim.corpora.dictionary import Dictionary
 
-from nltk.corpus import stopwords
-from gensim.matutils import cossim, sparse2full
-import numpy as np
 nltk.download('stopwords')
+print()
 
 texts=[]
+
+# Load document
 file = open("datasets/TokenVieuxM.txt", "r")
 lines = file.readlines()
 file.close()
 
-#print(stopwords.words())
-
 for line in lines:
-  line=line.strip()
-  lt=line.split(",")
-#Potential ill-character cleaning
-  for i in range(len(lt)):
-    lt[i]=lt[i].replace('[','')
-    lt[i]=lt[i].replace(']','')
-    lt[i]=lt[i].replace('"','')
-    lt[i]=lt[i].replace('\n','')
-    lt[i]=lt[i].replace(' ', '')
-#End : Potential ill-characters cleaning
-# print(lt)
-  ltc=[word for word in lt if not word in stopwords.words()]
-#  print("C", ltc)
-  texts.append(ltc)
+  # Parsing text and removing unwanted characters
+  line = re.findall(r"[a-zA-Z]+", line)
 
-#for text in texts:
-  #print(text)
+  # Remove stopwords  
+  words = [word for word in line if not word in stopwords.words()]
 
-#Here set the number of topics(to be changed if necessary)
+  texts.append(words)
+
+# Here set the number of topics(to be changed if necessary)
 nb=10
   
 id2word = Dictionary(texts)
 corpus = [id2word.doc2bow(text) for text in texts]
-#print(corpus)
 
-# Print dictionnary
-for i in id2word:
-  print(i, id2word[i])
-
-# Train the lda model on the corpus.
-from gensim.models import LdaModel
-lda = LdaModel(corpus, num_topics=nb)
+lda = LdaModel(
+  corpus, 
+  num_topics=nb, 
+  id2word=id2word, 
+  passes=1000,
+  alpha='auto', 
+  eta='auto',
+  decay=0.5, 
+  offset=1.0
+)
 
 # Print topic descrition
-for i in range(0, nb-1):
+for i in range(0, nb):
   value=lda.get_topic_terms(i)
-#  print(value)
-  print("Topic ", i)
+  print("Topic ", i+1)
   for j in value:
-    print(id2word[j[0]], " - P=", j[1])
+    word = id2word[j[0]]
+    print(f"P({word}) = {j[1]}")
   print()
 
-# Compute Perplexity
-perplexity_lda=lda.log_perplexity(corpus)  # a measure of how good the model is (lower the better).
-print('Perplexity= ', perplexity_lda)
+# Compute Perplexity, a measure of how good the model is (lower the better).
+perplexity_lda=lda.log_perplexity(corpus)  
+print(f'Perplexity = {perplexity_lda}')
 
 # Compute Coherence Score
-from gensim.models import CoherenceModel
 coherence_model_lda = CoherenceModel(model=lda, texts=texts, dictionary=id2word, coherence='c_v')
 coherence_lda = coherence_model_lda.get_coherence()
-print('Coherence= ', coherence_lda)
+print(f'Coherence = {coherence_lda}\n')
 
 # Find the most typical document for each topic
 # additional import
-from collections import defaultdict
-
-# ... your existing code ...
 
 # Compute topic proportions for each document
 doc_topics = lda.get_document_topics(corpus, minimum_probability=0)
@@ -93,4 +79,4 @@ for doc_index, doc_topic_proportions in enumerate(doc_topics):
 
 # Print most typical documents for each topic
 for topic_id, (doc_index, proportion) in most_typical_docs.items():
-    print(f"Most typical document for topic {topic_id} is document {doc_index} with proportion {proportion}")
+    print(f"Most typical document for topic {topic_id+1} is document {doc_index+1} with proportion {proportion}")
